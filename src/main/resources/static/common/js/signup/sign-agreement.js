8d72b879aaef8332688f198b101a9ec4c352848f
LitHope <107118066+LittleTwigs@users.noreply.github.com>
2023-06-08 16:33:51 +0900

    sign-agreement checkbox토글 기능 완료

--- src/main/resources/static/common/js/signup/sign-agreement.js
+++ src/main/resources/static/common/js/signup/sign-agreement.js
@@ -1,10 +1,49 @@
-   /*document.addEventListener("DOMContentLoaded", function() {*/
-            document.getElementById("continue").disabled = false;
-        function toggleDetails(id) {
+/*function toggleDetails(id) {
+	var details = document.getElementById(id);
+	details.classList.toggle("show");
+}
+
+function toggleAllCheckboxes() {
+	var childCheckboxes = document.querySelectorAll(".child-checkbox");
+	var allCheckbox = document.getElementById("all_agree");
+
+	childCheckboxes.forEach(function(checkbox) {
+		checkbox.checked = allCheckbox.checked;
+	});
+	validateForm();
+}
+
+function validateForm() {
+	var termsAgree = document.getElementById("terms_agree").checked;
+	var privacyAgree = document.getElementById("privacy_agree").checked;
+	var continueButton = document.getElementById("continue");
+
+	continueButton.disabled = !(termsAgree && privacyAgree);
+}
+function gotoNextPage() {
+	// 원래 페이지(샘플 페이지)의 URL을 변경하고 싶은 페이지로 변경합니다.
+	window.opener.location.href = "/racket/signauth";
+	// 팝업창을 닫습니다.
+	window.close();
+}
+
+function validateForm() {
+	var termsAgreeCheckbox = document.getElementById("terms_agree");
+	var privacyAgreeCheckbox = document.getElementById("privacy_agree");
+	var continueButton = document.getElementById("continue");
+
+	if (termsAgreeCheckbox.checked && privacyAgreeCheckbox.checked) {
+		continueButton.disabled = false;
+	} else {
+		continueButton.disabled = true;
+	}
+}*/
+
+function toggleDetails(id) {
             var details = document.getElementById(id);
             details.classList.toggle("show");
         }
-
+		
         function toggleAllCheckboxes() {
             var childCheckboxes = document.querySelectorAll(".child-checkbox");
             var allCheckbox = document.getElementById("all_agree");
@@ -16,16 +55,20 @@
         }
 
         function validateForm() {
-            var termsAgree = document.getElementById("terms_agree").checked;
-            var privacyAgree = document.getElementById("privacy_agree").checked;
+            var termsAgreeCheckbox = document.getElementById("terms_agree");
+            var privacyAgreeCheckbox = document.getElementById("privacy_agree");
             var continueButton = document.getElementById("continue");
 
-            continueButton.disabled = !(termsAgree && privacyAgree);
+            if (termsAgreeCheckbox.checked && privacyAgreeCheckbox.checked) {
+                continueButton.disabled = false;
+            } else {
+                continueButton.disabled = true;
+            }
         }
+
         function gotoNextPage() {
             // 원래 페이지(샘플 페이지)의 URL을 변경하고 싶은 페이지로 변경합니다.
             window.opener.location.href = "/racket/signauth";
             // 팝업창을 닫습니다.
             window.close();
         }
-/*   });*/
