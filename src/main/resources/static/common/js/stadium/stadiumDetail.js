7addb1da8d77c6ca726817bad37e6a8094ee5664
noyes5 <noyes5@naver.com>
2023-06-11 22:52:14 +0900

    구장 정보 페이지 레이아웃 수정 및 지도 추가

---
+++ src/main/resources/static/common/js/stadium/stadiumDetail.js
@@ -0,0 +1,42 @@
+function go_menu(targetClass) {
+    var targetElement = document.querySelector('.' + targetClass);
+    var menuElement = document.querySelector('.menu_home');
+    var offset = 180; // 상단과의 거리를 조정하는 픽셀 값
+
+    if (targetElement) {
+        var targetOffset = targetElement.offsetTop - offset;
+        window.scrollTo({
+            top : targetOffset,
+            behavior : "smooth"
+        });
+    }
+}
+/* 네이버 지도 관련 */
+
+var map = new naver.maps.Map('map', {
+    center : new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom : 15
+});
+
+var marker = new naver.maps.Marker({
+    position : new naver.maps.LatLng(37.3595704, 127.105399),
+    map : map
+});
+
+/* 사진 슬라이더 처리 */
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween : 30,
+    centeredSlides : true,
+    autoplay : {
+        delay : 2500,
+        disableOnInteraction : false,
+    },
+    pagination : {
+        el : ".swiper-pagination",
+        clickable : true,
+    },
+    navigation : {
+        nextEl : ".swiper-button-next",
+        prevEl : ".swiper-button-prev",
+    },
+});
