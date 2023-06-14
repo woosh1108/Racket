c1c58c450055146d21231cff7e21a63c33edf72e
ddt95 <ddt95@192.168.1.103>
2023-06-14 16:47:32 +0900

    DTO 및 컨트롤러,DAO,Service

--- src/main/java/com/multi/racket/controller/InquiryController.java
+++ src/main/java/com/multi/racket/controller/InquiryController.java
@@ -1,11 +1,35 @@
 package com.multi.racket.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.multi.racket.domain.InquiryDTO;
+import com.multi.racket.inquiry.InquiryService;
 @Controller
 public class InquiryController {
-	@RequestMapping("/inquiryboard")
-	public String inqboard() {
+	InquiryService service;
+	
+	@Autowired
+	public InquiryController(InquiryService service) {
+		super();
+		this.service = service;
+	}
+
+	@GetMapping("/inquiryboard")
+	public String inqboard(Model model) {
+		List<InquiryDTO> list = service.findAll();
+		model.addAttribute("inquirylist",list);
 		return "thymeleaf/inq/inquiryboard";
+	}
+	
+	@RequestMapping("/inquiryread")
+	public String inqread(int inquiryNo, Model model) {
+		InquiryDTO inquiry = service.read(inquiryNo);
+		model.addAttribute("inquiry", inquiry);
+		return "thymeleaf/inq/inquiryread";
 	}
 	
 	@RequestMapping("/inquirywrite")
@@ -13,14 +37,15 @@ public class InquiryController {
 		return "thymeleaf/inq/inquirywrite";
 	}
 	
+	@PostMapping("/inquiry_write")
+	public String write(InquiryDTO inquiry) {
+		service.insert(inquiry);
+		return "redirect:/inquiryboard";
+	}
+	
 	@RequestMapping("/report")
 	public String report() {
 		return "thymeleaf/inq/report";
-	}
-	
-	@RequestMapping("/inquiryread")
-	public String inqread() {
-		return "thymeleaf/inq/inquiryread";
 	}
 	
 	@RequestMapping("/admin_user")
@@ -32,4 +57,5 @@ public class InquiryController {
 	public String register() {
 		return "thymeleaf/inq/admin_register";
 	}
+		
 }
--- src/main/java/com/multi/racket/domain/InquiryDTO.java
+++ src/main/java/com/multi/racket/domain/InquiryDTO.java
@@ -1,7 +1,17 @@
 package com.multi.racket.domain;
+import java.util.Date;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +22,13 @@ import lombok.NoArgsConstructor;
 @Table(name = "inquiry")
 public class InquiryDTO {
 	@Id
-	private String inquiryNo;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int inquiryNo;
 	private String memberId;
 	private String inqTitle;
+	private String inqCategory;
 	private String inqContent;
-	private String inqDate;	
+	@UpdateTimestamp
+	@JsonFormat(pattern="yyyy-MM-dd",timezone="Asia/Seoul")
+	private Date inqDate;	
 }
