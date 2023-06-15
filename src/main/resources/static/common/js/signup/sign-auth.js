3eac599e3ee8177e50abf94a9806b69d4e99c105
LitHope <107118066+LittleTwigs@users.noreply.github.com>
2023-06-15 16:15:53 +0900

    이메일 인증기능 구현

--- src/main/resources/static/common/js/signup/sign-auth.js
+++ src/main/resources/static/common/js/signup/sign-auth.js
@@ -1,11 +1,12 @@
 $('#mail-send-btn').click(function() {
-		const eamil = $('#userEmail1').val() + $('#userEmail2').val(); // 이메일 주소값 얻어오기!
-		console.log('완성된 이메일 : ' + eamil); // 이메일 오는지 확인
+		const email = $('#userEmail1').val() + $('#userEmail2').val(); // 이메일 주소값 얻어오기!
+		console.log('완성된 이메일 : ' + email); // 이메일 오는지 확인
 		const checkInput = $('.mail-check-input') // 인증번호 입력하는곳 
 		
 		$.ajax({
 			type : 'get',
-			url : '<c:url value ="/racket/mailCheck?email="/>'+eamil, // GET방식이라 Url 뒤에 email을 뭍힐수있다.
+			url : '/racket/mailCheck', // GET방식이라 Url 뒤에 email을 뭍힐수있다.
+			data:"email="+email,
 			success : function (data) {
 				console.log("data : " +  data);
 				checkInput.attr('disabled',false);
@@ -13,7 +14,7 @@ $('#mail-send-btn').click(function() {
 				alert('인증번호가 전송되었습니다.')
 			}			
 		}); // end ajax
-	}); // end send eamil
+	}); // end send email
 	
 	// 인증번호 비교 
 	// blur -> focus가 벗어나는 경우 발생
@@ -25,8 +26,8 @@ $('#mail-send-btn').click(function() {
 			$resultMsg.html('인증번호가 일치합니다.');
 			$resultMsg.css('color','green');
 			$('#mail-Check-Btn').attr('disabled',true);
-			$('#userEamil1').attr('readonly',true);
-			$('#userEamil2').attr('readonly',true);
+			$('#userEmail1').attr('readonly',true);
+			$('#userEmail2').attr('readonly',true);
 			$('#userEmail2').attr('onFocus', 'this.initialSelect = this.selectedIndex');
 	         $('#userEmail2').attr('onChange', 'this.selectedIndex = this.initialSelect');
 		}else{
