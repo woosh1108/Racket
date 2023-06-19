46a8b932daef14a00af69a1576e7d569df3a02a1
woosh1108 <woosh1108@naver.com>
2023-06-19 10:47:03 +0900

    구장 예약하기 완성

---
+++ src/main/java/com/multi/racket/repository/CashServiceImpl.java
@@ -0,0 +1,35 @@
+package com.multi.racket.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.multi.racket.domain.CashDTO;
+
+@Service
+public class CashServiceImpl implements CashService {
+	private CashRepository cashRepository;
+
+    @Autowired
+    public CashServiceImpl(CashRepository cashRepository) {
+        this.cashRepository = cashRepository;
+    }
+
+	@Override
+	public CashDTO getLatestCashByMemberId(String memberId) {
+		// 최신 total_amount 가져오기
+        int totalAmount = cashRepository.findLatestTotalAmountByMemberId(memberId);
+        System.out.println("totalAmount : " + totalAmount);
+        // CashDTO 객체 생성 및 값 설정
+        CashDTO cash = new CashDTO();
+        cash.setMemberId(memberId);
+        cash.setTotalAmount(totalAmount);
+        
+        return cash;
+	}
+
+	@Override
+	public void saveCash(CashDTO cash) {
+		cashRepository.save(cash);
+	}
+
+}
