7dfaa3c0409db569afd594cff88cbf4a38f31fa7
ddt95 <ddt95@192.168.1.103>
2023-06-21 13:23:56 +0900

    commit

--- src/main/java/com/multi/racket/login/MemberDAO.java
+++
@@ -1,8 +0,0 @@
-package com.multi.racket.login;
-
-import com.multi.racket.domain.MemberDTO;
-
-public interface MemberDAO {
-	MemberDTO login(String memberId, String memberPass);
-	
-}
--- src/main/java/com/multi/racket/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.multi.racket.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.multi.racket.domain.MemberDTO;
-
-public interface MemberRepository extends JpaRepository<MemberDTO, Long> {
-	MemberDTO findByMemberIdAndMemberPass(String memberId,String memberPass);
-}
