54c9d18482326f9cac01c7baf63c4644a231c6cc
ddt95 <ddt95@192.168.1.103>
2023-07-05 16:05:11 +0900

    결제 기능

--- src/main/java/com/multi/racket/inquiry/InquiryService.java
+++ src/main/java/com/multi/racket/inquiry/InquiryService.java
@@ -1,9 +1,11 @@
 package com.multi.racket.inquiry;
 import com.multi.racket.domain.InquiryDTO;
+import com.multi.racket.domain.MemberDTO;
 public interface InquiryService {
 	public InquiryDTO insert(InquiryDTO dto);
 	public InquiryDTO read(int inquiryNo);
 	public InquiryDTO reply(InquiryDTO dto);
 	public void delete(int inquiryNo);
 	public void update(InquiryDTO dto);
+	public MemberDTO updatecash(String id, int updatecash);
 }
